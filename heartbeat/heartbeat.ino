a604bbaeca08919ca7ea04223b086378be7fe0dd
shell <shell@DESKTOP-PCNR08D.dhcp.wustl.edu>
2018-01-17 17:24:49 -0600

    heartbeat done

--- heartbeat/heartbeat.ino
+++ heartbeat/heartbeat.ino
@@ -1,9 +1,16 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(13, OUTPUT);
+
+  
 }
 
 void loop() {
   delay(1000);
+  digitalWrite(13, HIGH);
+  delay(10);
+  digitalWrite(13, LOW);
+  
   int time = (millis()+10)/1000;
   Serial.print(time);
   Serial.println(" sec have elapsed");
